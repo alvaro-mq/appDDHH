5313480d4be25b0e98da224824f62cc9a95361b4
nodejs <nodejs@agetic-i5-313.agetic-i5-313>
2016-12-21 14:55:33 -0400

    crecion de la estructura base del proyecto

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('appDdhh')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
